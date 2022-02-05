1f42ebe82600c0e24a8fb5f3058227d3b639a0de
Alex Cesca <alexcesca@gmail.com>
2022-02-05 00:19:24 -0300

    Adicionado a função para edição

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -45,3 +45,36 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 301)
 
 }
+func Edit(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	idConvertido, _ := strconv.Atoi(id)
+	produto := models.BuscaProduto(idConvertido)
+
+	temp.ExecuteTemplate(w, "Edit", produto)
+
+}
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		idConvertido, err := strconv.Atoi(id)
+		if err != nil {
+			log.Fatal("Erro na conversão do id:", err.Error())
+		}
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Fatal("Erro na conversão do preço:", err.Error())
+		}
+		quantidadeConvertida, errQtde := strconv.Atoi(quantidade)
+		if errQtde != nil {
+			log.Fatal("Erro na conversão da quantidade:", err.Error())
+		}
+		models.Update(idConvertido, nome, descricao, precoConvertido, quantidadeConvertida)
+		http.Redirect(w, r, "/", 301)
+	}
+
+}
--- models/produtos.go
+++ models/produtos.go
@@ -16,7 +16,7 @@ type Produto struct {
 
 func BuscaTodosProdutos() []Produto {
 	db := db.ConectaComBancoDeDados()
-	todos, err := db.Query("select * from produtos ")
+	todos, err := db.Query("select * from produtos order by id")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -33,18 +33,45 @@ func BuscaTodosProdutos() []Produto {
 		if err != nil {
 			panic(err.Error())
 		}
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
 		p.Quantidade = quantidade
-		p.Id = id
 		produtos = append(produtos, p)
 	}
 
 	defer db.Close()
 	return produtos
 }
+func BuscaProduto(id int) Produto {
+	db := db.ConectaComBancoDeDados()
+	sqlConsulta, err := db.Query("select * from produtos where id = $1 ", id)
+	if err != nil {
+		log.Fatal("Erro no insert dos produtos: ", err.Error())
+	}
 
+	produtoBanco := Produto{}
+
+	for sqlConsulta.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = sqlConsulta.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+		produtoBanco.Id = id
+		produtoBanco.Nome = nome
+		produtoBanco.Descricao = descricao
+		produtoBanco.Preco = preco
+		produtoBanco.Quantidade = quantidade
+	}
+
+	defer db.Close()
+	return produtoBanco
+}
 func Novo(nome string, descricao string, preco float64, quantidade int) {
 	db := db.ConectaComBancoDeDados()
 	sqlInsert, errInc := db.Prepare("insert into produtos( nome, descricao,preco,quantidade) values ($1, $2, $3, $4)")
@@ -61,5 +88,14 @@ func Deletar(id string) {
 		log.Fatal("Erro no insert dos produtos: ", errInc.Error())
 	}
 	sqlDelete.Exec(id)
+	defer db.Close()
+}
+func Update(id int, nome string, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+	sqlUpdate, errInc := db.Prepare("update produtos set nome = $1, descricao = $2, preco = $3, quantidade = $4 where  id = $5")
+	if errInc != nil {
+		log.Fatal("Erro no insert dos produtos: ", errInc.Error())
+	}
+	sqlUpdate.Exec(nome, descricao, preco, quantidade, id)
 	defer db.Close()
 }
